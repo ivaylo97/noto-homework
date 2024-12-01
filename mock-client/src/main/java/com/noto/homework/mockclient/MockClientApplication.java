a3edad3468c9b9bd4f0f824f17155306ee8b881b
Ivaylo Sapunarov <ivaylosapunarov@deltasource.eu>
2024-12-01 17:20:20 +0200

    Initial commit.

---
+++ mock-client/src/main/java/com/noto/homework/mockclient/MockClientApplication.java
@@ -0,0 +1,13 @@
+package com.noto.homework.mockclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockClientApplication.class, args);
+	}
+
+}
---
+++ mock-client/src/test/java/com/noto/homework/mockclient/MockClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noto.homework.mockclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
