ed894849563229ad2f41023f263fb5eeb4d1c120
Ivaylo Sapunarov <ivaylosapunarov@deltasource.eu>
2024-12-02 08:36:15 +0200

    Added dockerfile, compose file and docker-entrypoint

--- mock-client/src/main/java/com/noto/homework/mockclient/MockClientApplication.java
+++ mock-client/src/main/java/com/noto/homework/mockclient/MockClientApplication.java
@@ -1,8 +1,13 @@
 package com.noto.homework.mockclient;
 
+import com.noto.homework.mockclient.beans.countryconfiguration.CountryLocationConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableConfigurationProperties(CountryLocationConfiguration.class)
 @SpringBootApplication
 public class MockClientApplication {
 
@@ -10,4 +15,8 @@ public class MockClientApplication {
 		SpringApplication.run(MockClientApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
